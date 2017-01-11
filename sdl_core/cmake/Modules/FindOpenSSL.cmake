c30d0c8ab3baa9001bc07559e7ad1614e71649d2
durandaltheta <blaynetdennis@gmail.com>
2017-01-11 16:56:50 -0600

    removed accidental clone of repository

--- sdl_core/cmake/Modules/FindOpenSSL.cmake
+++
@@ -1,2 +0,0 @@
-set(OPENSSL_INCLUDE_DIR ${CMAKE_SYSROOT}/usr/include/gnutls)
-set(OPENSSL_LIBRARIES ${CMAKE_SYSROOT}/usr/lib/libssl.so)
--- sdl_core/export_arm_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-export CMAKE_SYSROOT="/opt/poky-agl/3.0.0+snapshot/sysroots/cortexa7hf-neon-vfpv4-agl-linux-gnueabi/"
-export CFLAGS='-mfloat-abi=hard'
-export CXXFLAGS='-mfloat-abi=hard'
-export LDFLAGS=" -L$CMAKE_SYSROOT/usr/lib/ -L$CMAKE_SYSROOT/lib/"
-LD_LIBRARY_PATH="$CMAKE_SYSROOT/usr/lib:$CMAKE_SYSROOT/lib:"$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH
